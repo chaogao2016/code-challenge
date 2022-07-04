eb65cf033c4b9223fc808831f4cde9e51021141b
高超 <gaoc@weipaitang.com>
2022-07-04 23:35:26 +0800

    注释

--- config/db.php
+++ config/db.php
@@ -2,9 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=wpt',
-    'username' => 'wpt',
-    'password' => 'wptpwd',
+    'dsn' => getenv('DB_DSN'),
+    // 'dsn' => 'mysql:host=127.0.0.1;dbname=wpt',
+    'username' => getenv('DB_USERNAME'),
+    // 'username' => 'wpt',
+    'password' => getenv('DB_PASSWORD'),
+    // 'password' => 'wptpwd',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
