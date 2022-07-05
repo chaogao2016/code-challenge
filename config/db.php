06c15823a26e0ca9459b1d56fbcc3feb959621b1
高超 <gaoc@weipaitang.com>
2022-07-05 11:48:55 +0800

    修改数据库配置

--- config/db.php
+++ config/db.php
@@ -3,12 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => getenv('DB_DSN'),
-    // 'dsn' => 'mysql:host=127.0.0.1;dbname=wpt',
     'username' => getenv('DB_USERNAME'),
-    // 'username' => 'wpt',
     'password' => getenv('DB_PASSWORD'),
-    // 'password' => 'wptpwd',
-    'charset' => 'utf8',
 
     // Schema cache options (for production environmen t)
     //'enableSchemaCache' => true,
